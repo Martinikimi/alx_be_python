f36c4992ae57a300f915710885e9706e51f9b23e
Martin Kimani <martinikimi7@gmail.com>
2025-08-03 23:07:49 +0300

    Fix test method names and structure

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,4 +1,3 @@
-
 # programming_paradigm/test_simple_calculator.py
 import unittest
 from simple_calculator import SimpleCalculator
@@ -14,7 +13,9 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5, 3), 2)
 
     def test_multiply(self):
+        """Test multiplication of two numbers."""
         self.assertEqual(self.calc.multiply(4, 3), 12)
 
     def test_divide(self):
+        """Test division of two numbers."""
         self.assertEqual(self.calc.divide(10, 2), 5)

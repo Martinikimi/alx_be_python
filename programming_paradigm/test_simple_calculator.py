e8febc01fed3bea27004b8fddbbce9446e0fdad4
Martin Kimani <martinikimi7@gmail.com>
2025-08-03 23:11:45 +0300

    Rename test methods to match ALX checker requirements

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,10 +12,10 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         """Test multiplication of two numbers."""
         self.assertEqual(self.calc.multiply(4, 3), 12)
 
-    def test_divide(self):
+    def test_division(self):
         """Test division of two numbers."""
         self.assertEqual(self.calc.divide(10, 2), 5)

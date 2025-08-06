11ea7bcc016f5946cac0bbcf4829a355bc7d6272
Martin Kimani <martinikimi7@gmail.com>
2025-08-06 17:02:05 +0300

    Update main.py

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,17 +1,26 @@
 import sys
-from robust_division_calculator import safe_divide
+
+def safe_divide(numerator, denominator):
+    try:
+        result = numerator / denominator
+        return result
+    except ZeroDivisionError:
+        return f"Error: Cannot divide by zero."
 
 def main():
     if len(sys.argv) != 3:
         print("Usage: python main.py <numerator> <denominator>")
-        sys.exit(1)
-
-    numerator = sys.argv[1]
-    denominator = sys.argv[2]
+        return
+    
+    try:
+        numerator = float(sys.argv[1])
+        denominator = float(sys.argv[2])
+    except ValueError:
+        return f"Error: Please enter numeric values only."
+        return
 
     result = safe_divide(numerator, denominator)
-    print(result)
+    print("The result of the division is", result)
 
 if __name__ == "__main__":
     main()
-
